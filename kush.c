4612bea4206bc259057efdb9383fe8fe317dd56d
ojhak02 <49183499+ojhak02@users.noreply.github.com>
2019-04-02 12:13:27 +0530

    Create kush.c

---
+++ kush.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%2==0)
+      printf("no is even");
+      else
+      printf("nu is odd")
+ }     
